b840b7d91bdcc99b46c855b97156f6789c8c0bcb
kimjihong9 <rlawyd9@kookmin.ac.kr>
2020-11-05 05:55:10 +0900

    makeITApplication

---
+++ MakeITApplication/app/src/main/java/kr/ac/kookmin/makeit/FragmentHome.java
@@ -0,0 +1,77 @@
+package kr.ac.kookmin.makeit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentHome#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentHome extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FragmentHome() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static FragmentHome newInstance(String param1, String param2) {
+        FragmentHome fragment = new FragmentHome();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+
+        // 현재 로그인된 ID로 표시
+        String userId = SaveSharedPreference.getUserName(getContext());
+        TextView textUser = (TextView) rootView.findViewById(R.id.text_username);
+        textUser.setText(userId);
+
+        // 찜 목록 리스트 보여주기(클릭 이벤트 추가)
+        Button btnBookmark = (Button) rootView.findViewById(R.id.btn_bookmark_list);
+        btnBookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 찜목록 액티비티로 전환
+                Intent intent = new Intent(getContext(), BookmarkActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
+    }
+}
